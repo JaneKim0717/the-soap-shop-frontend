9608ffa8fa64be5c76e41fdcbe443df540046f94
Jane Kim <jane.kim0717@gmail.com>
2022-05-09 15:17:18 -0400

    Styling updates to order summary page

--- src/components/pages/cart-page/CartItems.js
+++ src/components/pages/cart-page/CartItems.js
@@ -1,7 +1,100 @@
 import React from 'react'
+import styled from "styled-components";
 import AddIcon from '@mui/icons-material/Add';
 import RemoveIcon from '@mui/icons-material/Remove';
 import DeleteOutlineIcon from '@mui/icons-material/DeleteOutline';
+
+const Wrapper = styled.div`
+  padding: 20px;
+`;
+
+const Bottom = styled.div`
+  display: flex;
+  justify-content: space-between;
+`;
+
+const Info = styled.div`
+  flex: 3;
+`;
+
+const Product = styled.div`
+  display: flex;
+  justify-content: space-between;
+`;
+
+const ProductDetail = styled.div`
+  flex: 2;
+  display: flex;
+`;
+
+const Image = styled.img`
+  width: 110px;
+`;
+
+const Details = styled.div`
+  padding: 20px;
+  display: flex;
+  flex-direction: column;
+  justify-content: space-around;
+`;
+
+const ProductName = styled.span``;
+
+const PriceDetail = styled.div`
+  flex: 1;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+`;
+
+const ProductAmountContainer = styled.div`
+  display: flex;
+  align-items: center;
+  margin-bottom: 20px;
+`;
+
+const ProductPrice = styled.div`
+  font-size: 1rem;
+  font-weight: normal;
+`;
+
+const Hr = styled.hr`
+  background-color: #eee;
+  border: none;
+  height: 1px;
+`;
+
+const ButtonsContainer = styled.div`
+  border: none;
+  padding: 0.5rem 1rem;
+  cursor: pointer;
+  font-size: 10px;
+  margin-right: 5px;
+  margin-top: 1.3rem;
+`
+
+const ButtonIncrease = styled.button`
+  border: none;
+  color: rgb(14, 14, 14);
+  background-color: white;
+  cursor: pointer;
+`
+
+const ButtonDecrease = styled.button`
+  border: none;
+  color: rgb(14, 14, 14);
+  background-color: white;
+  cursor: pointer;
+`
+
+const ButtonTrash = styled.button`
+  border: none;
+  color: rgb(14, 14, 14);
+  background-color: white;
+  cursor: pointer;
+`
+
 
 
 
@@ -12,33 +105,84 @@ function CartItems(props) {
 
 
   return (
-    <>
-      <div className="cart-item">
-        <div className="item-image">
-          <img src={image} alt='product'/>
-        </div>
-        <div className="name-price">
-          <h4>{name}</h4>
-          <p>${price}.00</p>
-        </div>
-        <div className="quantity">
-          <p>{`QUANTITY: ${quantity}`}</p>
-        </div>
-        <div className="btns-container">
-          <button className="btn-increase" onClick={() => increase(product)}>
-            <AddIcon width='20px' />
-          </button>
 
-          <button className="btn-decrease" onClick={() => decrease(product)} >
-            <RemoveIcon width='20px' />
-          </button>
+    <Wrapper>
+      <Bottom>
+        <Info>
+          <Product>
+            <ProductDetail>
+              <Image src={image} />
+              <Details>
+                <ProductName>
+                  {name}
+                </ProductName>
+              </Details>
+            </ProductDetail>
+            <PriceDetail>
+              <ProductPrice>{`QUANTITY: ${quantity}`}</ProductPrice>
+            </PriceDetail>
+            <PriceDetail>
+              <ProductPrice>${price}.00</ProductPrice>
+            </PriceDetail>
+            <ProductAmountContainer>
+              <ButtonsContainer>
+                <ButtonIncrease onClick={() => increase(product)}>
+                  <AddIcon width='20px' />
+                </ButtonIncrease>
 
-          <button className="btn-trash" onClick={() => removeProduct(product)}>
-            <DeleteOutlineIcon width='20px' />
-          </button>
-        </div>
-      </div>
-    </>
+                <ButtonDecrease onClick={() => decrease(product)} >
+                  <RemoveIcon width='20px' />
+                </ButtonDecrease>
+
+                <ButtonTrash onClick={() => removeProduct(product)}>
+                  <DeleteOutlineIcon width='20px' />
+                </ButtonTrash>
+
+              </ButtonsContainer>
+            </ProductAmountContainer>
+          </Product>
+          <Hr />
+
+        </Info>
+
+      </Bottom>
+    </Wrapper>
+
+
+
+
+
+
+
+
+
+    // <>
+    //   <div className="cart-item">
+    //     <div className="item-image">
+    //       <img src={image} alt='product'/>
+    //     </div>
+    //     <div className="name-price">
+    //       <h4>{name}</h4>
+    //       <p>${price}.00</p>
+    //     </div>
+    //     <div className="quantity">
+    //       <p>{`QUANTITY: ${quantity}`}</p>
+    //     </div>
+    //     <div className="btns-container">
+    //       <button className="btn-increase" onClick={() => increase(product)}>
+    //         <AddIcon width='20px' />
+    //       </button>
+
+    //       <button className="btn-decrease" onClick={() => decrease(product)} >
+    //         <RemoveIcon width='20px' />
+    //       </button>
+
+    //       <button className="btn-trash" onClick={() => removeProduct(product)}>
+    //         <DeleteOutlineIcon width='20px' />
+    //       </button>
+    //     </div>
+    //   </div>
+    // </>
   )
 }
 
--- src/components/pages/cart-page/Total.js
+++ src/components/pages/cart-page/Total.js
@@ -1,23 +1,144 @@
 import React from 'react'
 import { withRouter } from 'react-router-dom'
+import styled from "styled-components";
+
+const Wrapper = styled.div`
+  padding: 20px;
+`;
+
+const Bottom = styled.div`
+  display: flex;
+  justify-content: space-between;
+`;
+
+const Summary = styled.div`
+  flex: 1;
+  border: 0.5px solid lightgray;
+  padding: 20px;
+  height: 40vh;
+  width: 400px;
+`;
+
+const SummaryTitle = styled.h1`
+  font-weight: 300;
+  margin-bottom: 50px;
+  text-align: center;
+`;
+
+const SummaryItem = styled.div`
+  width: 400px;
+  margin: 30px 0px;
+  display: flex;
+  justify-content: space-between;
+  font-weight: ${(props) => props.type === "total" && "500"};
+  font-size: ${(props) => props.type === "total" && "24px"};
+`;
+
+const SummaryItemText = styled.span`
+  font-size: 1em;
+`;
+
+const SummaryItemTextTotal = styled.span`
+  font-size: 1.2em;
+  font-weight: bold;
+`;
+
+const SummaryItemPrice = styled.span`
+  font-size: 1em;
+`;
+
+const SummaryItemPriceTotal = styled.span`
+  font-size: 1.2em;
+  font-weight: bold;
+`;
+
+const Button = styled.button`
+  width: 100%;
+  padding: 10px;
+  background-color: black;
+  color: white;
+  font-weight: 600;
+  cursor: pointer;
+  margin-top: 0.5rem;
+  border: none;
+`;
+
+const ButtonClearCart = styled.button`
+  width: 100%;
+  padding: 10px;
+  background-color: #f5f5f5;
+  color: black;
+  font-weight: 600;
+  cursor: pointer;
+  margin-top: 0.5rem;
+  border: none;
+`;
+
 
 
 function Total({ itemCount, total, history, clearCart }) {
   return (
-    <div className='order-summary'>
-      <div className="total-container">
-        <div className="total">
-          <h3>Total Items: {itemCount}</h3>
-          <h3>{`Total Amount: $${total}.00`}</h3>
-        </div>
-        <div className="checkout">
-          <button className="button is-black" onClick={() => history.push('/checkout')}>CHECKOUT</button>
-        </div>
-        <div className="checkout">
-          <button className="button is-white" onClick={() => clearCart()}>CLEAR CART</button>
-        </div>
-      </div>
-    </div>
+    <Wrapper>
+      <Summary>
+        <SummaryTitle>ORDER SUMMARY</SummaryTitle>
+          <Bottom>
+
+            <div className='order-summary'>
+              <div className="total-container">
+
+              <SummaryItem>
+                <SummaryItemText>ITEM COUNT:</SummaryItemText>
+                <SummaryItemPrice>{itemCount}</SummaryItemPrice>
+              </SummaryItem>
+
+              <SummaryItem>
+                <SummaryItemText>SUBTOTAL:</SummaryItemText>
+                <SummaryItemPrice>${total}.00</SummaryItemPrice>
+              </SummaryItem>
+
+              <SummaryItem>
+                <SummaryItemText>FREE SHIPPING:</SummaryItemText>
+                <SummaryItemPrice>$0.00</SummaryItemPrice>
+              </SummaryItem>
+
+              <SummaryItem>
+                <SummaryItemTextTotal>TOTAL AMOUNT:</SummaryItemTextTotal>
+                <SummaryItemPriceTotal>${total}.00</SummaryItemPriceTotal>
+              </SummaryItem>
+
+              <Button onClick={() => history.push('/checkout')}>CHECKOUT</Button>
+
+              <ButtonClearCart onClick={() => clearCart()}>CLEAR CART</ButtonClearCart>
+
+            </div>
+          </div>
+        </Bottom>
+      </Summary>
+    </Wrapper>
+
+
+
+
+
+
+
+
+
+
+    // <div className='order-summary'>
+    //   <div className="total-container">
+    //     <div className="total">
+    //       <h3>Total Items: {itemCount}</h3>
+    //       <h3>{`Total Amount: $${total}.00`}</h3>
+    //     </div>
+    //     <div className="checkout">
+    //       <button className="button is-black" onClick={() => history.push('/checkout')}>CHECKOUT</button>
+    //     </div>
+    //     <div className="checkout">
+    //       <button className="button is-white" onClick={() => clearCart()}>CLEAR CART</button>
+    //     </div>
+    //   </div>
+    // </div>
   )
 }
 
