37b567e8e66ca5dc1d6cf7902692bf898f5399d3
Jane Kim <jane.kim0717@gmail.com>
2022-05-10 13:41:36 -0400

    Styling updates

--- src/components/Filter.js
+++ src/components/Filter.js
@@ -15,14 +15,14 @@ const FilterWrap = styled.div`
 const FilterText = styled.span`
   font-size: 15px;
   font-weight: 600;
-	margin-left: 120px;
+	margin-left: 150px;
   margin-right: 5px;
 `;
 
 const Select = styled.select`
   padding: 10px;
   margin-right: 20px;
-	width: 200px;
+	width: 270px;
 	border: 1px solid rgba(0, 0, 0, 0.125);
 
 `;
